97715f80e9d54273fafb7428e006cf58905d6886
Anastasiia Konstantynova <developer1302@gmail.com>
2021-07-05 19:08:05 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,7 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 function onInputChange() {
-  return (nameOutput.textContent = nameInput.value);
-}
+  return nameOutput.textContent = nameInput.value ? nameInput.value : 'незнакомец';
+};
+
 nameInput.addEventListener('input', onInputChange);
